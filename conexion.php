98fda83be092397c12a405c6d223e8992f3664b3
Zidanearroyo <167923327+Zidanearroyo@users.noreply.github.com>
2024-05-21 20:31:27 -0600

    Hosting test 1
    Primer prueba de hosting
    (Pagina carga pero no hay conexión con la base de datos)

--- conexion.php
+++ conexion.php
@@ -1,5 +1,5 @@
 <?php 
 
-$conex = mysqli_connect("localhost","root","","dbproyecto");
+$conex = mysqli_connect("sql101.infinityfree.com","if0_36150775","Fi0qF5lzREfqu","if0_36150775_dbproyecto");
 
 ?>
